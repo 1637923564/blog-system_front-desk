cba14923d92b559516ceb584f45c85b16766795a
1637923564 <1637923564@qq.com>
2021-04-09 11:19:47 +0800

    n

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-
+  <Header />
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
   setup() {
 
+  },
+  components: {
+    Header
   }
 }
 </script>
-
-<style lang="scss">
-
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="header-body">
+      <div class="header-logo">
+        <img src="../assets/image/logo.png">
+      </div>
+      <div class="header-nav">
+        nav
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/tool';
+
+header {
+  height: 90px;
+
+  .header-body {
+    height: 100%;
+    width: $content-size;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .header-logo {
+      img {
+        height: 40px;
+      }
+    }
+    .header-nav {
+
+    }
+  }
+}
+</style>
